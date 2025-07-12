8ac9984aee40ac09a14178be11b26c919d3864f9
Kushal J <jkushal208@gmail.com>
2025-07-12 16:03:33 +0530

    functionality update

---
+++ src/script.js
@@ -0,0 +1,48 @@
+
+let startbtn=document.getElementById("startbutton")
+let options=document.getElementById("options")
+let nextBtn=document.getElementById("nextBtn")
+let questionDisp=document.getElementById("Question")
+let currentIndex=0;
+
+
+
+let questions=[{
+  question:"Which are the following is not a christopherNolan Movie",
+  choice:["Interstellar","Inception","Dune","Oppenhemier"],
+  answer:"Dune"
+
+},
+{
+  question:"Which is the top Grossing MCU Movie",
+  choice:["InfinityWar","EndGame","ThorRagnork","CivilWar"],
+  answer:"Endgame"
+}
+
+
+]
+
+function startQuestion(){
+
+}
+
+function renderQuestion(){
+
+  let Question= questions[currentIndex]
+
+  
+
+
+
+
+
+
+
+}
+
+function nextQuestion(){
+
+}
+
+
+
